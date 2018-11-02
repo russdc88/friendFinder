e3508337ad89181e93a4c0f93bb6166348d7df25
russdc88 <russdc88@gmail.com>
2018-11-02 15:33:25 -0600

    code finished

---
+++ app/data/friend.js
@@ -0,0 +1,20 @@
+//Where we will store our friends data.
+
+
+var friendsArray = [ {
+	name: "Russ",
+	photo:"http://cdn7.viralscape.com/wp-content/uploads/2015/06/Man-And-Cat-9.jpg",
+	score: [
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		6,
+		7,
+		8
+	]
+}];
+
+module.exports = friendsArray;
